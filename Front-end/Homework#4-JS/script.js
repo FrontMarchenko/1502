5da807c0e1de10b556c3ecc1649d5c19707e97a0
VolodymyrMarchenko <nokialeader@gmail.com>
2018-03-14 14:28:19 +0200

    added homework JS

---
+++ Front-end/Homework#4-JS/script.js
@@ -0,0 +1,14 @@
+"use strict"
+var mark= prompt ("\"Введите число от 2 до 5\"");
+if (mark==2) {
+            alert('Плохо');
+}   else if (mark==3) {
+            alert('Средне');
+}   else  if (mark==4) {
+            alert ('Хорошо');
+}   else  if (mark==5) {
+            alert ('Отлично');
+}		else {
+						alert ('Ошибка');
+}
+					
